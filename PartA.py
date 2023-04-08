af1bf5e1cb7d6d2435bf2c1b2e04ee4ca173f1c5
Aileen Tran <aileenyt@uci.edu>
2023-04-08 15:14:07 -0700

    worked on computeWordFrequencies

--- PartA.py
+++ PartA.py
@@ -13,11 +13,25 @@ def tokenize(TextFilePath):
         for word in line.split(" "):
             word = word.lower()
             token.append(word)
+    #setToken = {token}
+    return list(set(token))
+
 
 TextFilePath = "C:\\Users\\ailee\\Desktop\\in4matx 141\\testA.txt"
-tokenize(TextFilePath)
+
 
 #Map<Token,Count> computeWordFrequencies(List,Token>)
 
+def computeWordFrequencies(token_list):
+    map_token = dict()
+    for word in token_list:
+        if word in map_token:
+            value = map_token(word)
+            map_token[word] = value+1 
+        else:
+            map_token[word] = 1
+    return map_token
 
+
+print(computeWordFrequencies(tokenize(TextFilePath)))
 #void print(Frequencies<Token,Count>)
