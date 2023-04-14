94c75a1212941cbc4217c280ce32dd6d55ddb1f7
Aileen Tran <aileenyt@uci.edu>
2023-04-13 22:29:22 -0700

    editted tokenize to handle special cases and timed it

--- PartA.py
+++ PartA.py
@@ -1,25 +1,34 @@
-
-
+import re
+import time
 #runtime complexity explanation linear? polynomial? exponential? 
 #[delete later] token: sequence of alphanumeric characters indepednent of
 #capitalization
 
 #List<Token> tokenize(TextFilePath)
 def tokenize(TextFilePath):
-    file = open(TextFilePath)
     token = []
+    try:
+        with open(TextFilePath) as file:
+            for line in file:
+                for word in line.split(" "):
+                    if not word.isalnum():
+                        tempt_list = re.split(r"[^A-Z^a-z^0-9]", word)
+                        tempt_list = [i for i in tempt_list if i.isalnum()]
+                        token = token + tempt_list
+                        
+                    else:
+                        token.append(word)
+        #setToken = {token}
+        print(token)
+        return token
+    except FileNotFoundError as e:
+        print("File not found")
 
-    for line in file:
-        for word in line.split(" "):
-            word = word.lower()
-            token.append(word)
-    #setToken = {token}
-    return list(set(token))
-
-
+start_time = time.time()
 TextFilePath = "C:\\Users\\ailee\\Desktop\\in4matx 141\\testA.txt"
-
-
+tokenize(TextFilePath)
+stop_time = time.time() 
+print(stop_time-start_time)
 #Map<Token,Count> computeWordFrequencies(List,Token>)
 
 def computeWordFrequencies(token_list):
@@ -33,5 +42,10 @@ def computeWordFrequencies(token_list):
     return map_token
 
 
-print(computeWordFrequencies(tokenize(TextFilePath)))
+#map_token = computeWordFrequencies(tokenize(TextFilePath))
+#print(map_token)
 #void print(Frequencies<Token,Count>)
+#def Frequencies(map_token):
+#    print(list(map_token))
+
+#print(Frequencies(map_token))
