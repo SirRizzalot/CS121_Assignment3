c28b2ee0279ef582f93a29421d85095154f33e9a
Aileen Tran <aileenyt@uci.edu>
2023-04-14 21:54:03 -0700

    did PartB

--- PartA.py
+++ PartA.py
@@ -1,25 +1,23 @@
 import re
 import time
 import sys
-#runtime complexity explanation linear? polynomial? exponential? 
-#[delete later] token: sequence of alphanumeric characters indepednent of
-#capitalization
 
-#List<Token> tokenize(TextFilePath)
+#
+#
+#
+#
 def tokenize(TextFilePath):
     token = []
     try:
-        with open(TextFilePath) as file:
+        with open(TextFilePath, encoding = "utf8") as file:
             for line in file:
                 for word in line.split(" "):
-                    if not word.isalnum():
-                        tempt_list = re.split(r"[^A-Z^a-z^0-9]", word)
+                    if not word.isalnum():                        
+                        tempt_list = re.split(r"[^A-Z^a-z^0-9]", word)                        
                         tempt_list = [i for i in tempt_list if i.isalnum()]
-                        token = token + tempt_list
-                        
+                        token = token + tempt_list                       
                     else:
                         token.append(word)
-        #setToken = {token}
         return token
     except FileNotFoundError as e:
         print("File not found")
@@ -56,14 +54,11 @@ def Frequencies(map_token):
 
 #line 61 referenced from https://stackoverflow.com/questions/1009860/how-can-i-read-and-process-parse-command-line-arguments
 def main():
-    start_time = time.time()
-
     TextFilePath = sys.argv[1]
+    
     map_token = computeWordFrequencies(tokenize(TextFilePath))
+    
     Frequencies(map_token)
-
-    stop_time = time.time() 
-    print(f"time {stop_time-start_time}")
 
 if __name__ == "__main__":
     main()
