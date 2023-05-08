d60c53936a9fec9c22531e387e61495838b290e3
Lance Li <124323253+SirRizzalot@users.noreply.github.com>
2023-05-08 11:29:53 -0700

    initiation

---
+++ website_indexer.py
@@ -0,0 +1,17 @@
+######################################################################################################################
+# functionality:
+#    for each website, find all the words in the content and use each word as a key and add the url to it's value
+# input:
+#   a folder containing json files
+# output:
+#   a text file where it starts with the key then ":" then [urls]
+# example outputs
+#   apple: [apple.com, appleAreNice.com]
+#   pineapple: [pineapple.com, pineappleAreNice.com]
+######################################################################################################################
+
+def main():
+   pass
+
+if __name__ == "__main__":
+   main()
---
+++ word_indexer.py
@@ -0,0 +1,21 @@
+######################################################################################################################
+# functionality:
+#    for key value pairs in the website_indexer file,
+#    use the key as this file's key and value as the line in which the key is in
+# input:
+#   a text file containing word:[url] pairs
+# output:
+#   a text file containing word:integer pairs
+# example outputs
+#   apple: 0
+#   pineapple: 1
+######################################################################################################################
+
+
+import sys
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
