f1bb3ca99e62eabe44dad7f0297688105970e314
Anthony Wen <awen5@uci.edu>
2023-05-19 11:01:19 -0700

    fixed retrieval.py

--- Retrieval.py
+++ Retrieval.py
@@ -15,44 +15,79 @@
 # from sys import argv
 # import time
 
+from collections import defaultdict
 import csv
-
-def load_data(file_name, separator):
+def load_datatxt(file_name):
     data = {}
-    with open(file_name, newline='') as file:
-        reader = csv.reader(file, delimiter=separator)
-        for row in reader:
-            key = row[0]
-            value = row[1:]
+    with open(file_name) as file:
+        for line in file:
+            line = line.strip()
+            key, value = line.split(' : ')
             data[key] = value
     return data
 
-def load_website(keys):
-    data = []
-    with open('website_index.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            key = row[0]
-            if key in keys:
-                data.append(row[1])
+def load_urlId(file_name):
+    data = {}
+    with open(file_name) as file:
+        for line in file:
+            line = line.strip()
+            
+            key, value = line[1:-1].split(': ')
+            data[int(key)] = value
     return data
-                
+
+def load_websitetxt(filename, text):
+    data = defaultdict(list)
+    keys = text.split()
+    with open(filename) as file:
+        for line in file:
+            line = line.strip()
+            key, value = line.split(': ')
+
+            if key in keys:
+                value1 = value[1:-1].replace("'","").split(", ")
+
+                for i in value1:
+                    data[key].append(i.split(","))
+    return data 
+
+def get_urls(data):
+    url_nums = []
+    for _, value in data.items():
+        for elements in value:
+            # print(elements[2])
+            url_nums.append(int(elements[2]))
+    url_nums.sort()
+
+    # print(url_nums)
+
+    urls = []
+    url_data = load_urlId('url_ids.txt')
+    # print(url_data)
+    for num in url_nums:
+        urls.append(url_data[num])
+
+
+    return urls
 
 
 
 
 
 if __name__ == '__main__':
-    # url_data = load_data('url_ids.csv', ',')
-    # word_data = load_data('word_index_locator.csv', ',')
-    # website_data =load_data('website_index.csv', ',')
-    keys = ["two", "which", "uci"]
-    website_data = load_website(keys)
-    # print("URL Data:")
+    # url_data = load_urlId('url_ids.txt')
     # print(url_data)
-    # print("\nWord Data:")
+
+    # word_data = load_datatxt('word_index_locator.txt')
     # print(word_data)
-    print("Website Data:")
+
+    text = "zyl zzxyz"
+    website_data = load_websitetxt('website_index.txt', text)
     print(website_data)
+
+    # urls = get_urls(website_data)
+    # print(urls)
+
+    
 
 
