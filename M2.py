8c0a36802071f37011a92e4372c3f4d275a17422
Van Pham <thyvan1412@gmail.com>
2023-06-02 02:38:41 -0700

    .

--- M2.py
+++ M2.py
@@ -4,6 +4,7 @@ from Ranking import *
 import itertools  # for slicing dictionary
 import time
 
+
 ######################################################################################################################
 # team members:
 # Aileen Tran aileenyt, 79192463
@@ -25,7 +26,8 @@ if __name__ ==  "__main__":
     start = time.time()
     score_list = {}  # tfidf scores
     special_score_list = {}  # special case scores
-    
+     
+    start2 = time.time()
     for word in query1.query:
         word_rank = Ranker(word, query1)
         # get tfidf score
@@ -34,7 +36,9 @@ if __name__ ==  "__main__":
         # get special score
         word_rank.get_special_score()
         special_score_list[word] = word_rank.special_score
+    end2 = time.time()
     
+    start3 = time.time()
     # get query tfidf score
     query_score = get_tf_idf_of_query_words(query1.query, query1)
     #dictionary format of score of words in query for all urls having them
@@ -43,6 +47,7 @@ if __name__ ==  "__main__":
     #     url2: {score_w1, score_w2, score_w3},
     #     url3: {score_w1, score_w2, score_w3},
     # }
+    
     all_doc_vector = {}
     for word in query1.query:
         for url in query1.parent.url_id_to_string:
@@ -52,39 +57,48 @@ if __name__ ==  "__main__":
                 else:
                     all_doc_vector[url] = []
                     all_doc_vector[url].append(score_list[word][url])
-
+    
     # add score 0 to words not appear in url but appears in query
     for url in all_doc_vector:
         if len(all_doc_vector[url]) < len(query1.query):
             for i in range(len(query1.query) - len(all_doc_vector[url])):
                 all_doc_vector[url].append(0)
+    end3 = time.time()   
         
-        
+    start4 = time.time()
     # the dictionary of cosine similarities score for all docs containing at least 1 word in the query
     cos_sim_list = {}    
     for doc in all_doc_vector:
         cos_sim = compute_cosine_similarities(query_score, all_doc_vector[doc])
         cos_sim_list[doc] = -cos_sim
-     
-     
+    end4 = time.time()
+    
+    start5 = time.time()
     # add special word cases to score after calculating cosine similarities   
     for word in special_score_list:
         for url, special_score in special_score_list[word].items():
-            cos_sim_list[url] += special_score
-        
-        
+            cos_sim_list[url] += special_score  
+    end5 = time.time()    
+       
+    start6 = time.time() 
     # sort the cosine similarities score dictionaries
     cos_sim_list = sorturl(cos_sim_list)
-    print("cos_sim_list", cos_sim_list)
+    #print("cos_sim_list", cos_sim_list)
     # take the top 5 urls
     if len(cos_sim_list) >= 5:
         top5 = dict(itertools.islice(cos_sim_list.items(), 5))
     else:
         top5 = cos_sim_list
-        
+    
+    end6 = time.time()    
     
     # output the top 5 urls
     for k,v in top5.items():
         print(query1.parent.url_id_to_string[k])  
     end = time.time()
     print("time", end - start)
+    print("time do word_rank", end2 - start2)
+    print("time format doc_vector", end3 - start3)
+    print("time calculate cosine", end4 - start4)
+    print("time add extra weight", end5 - start5)
+    print("time sort", end6 - start6)
