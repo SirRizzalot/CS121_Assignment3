3d1e1e29325f6aad6ad58d6c6ef33bc66cb9d338
awen5 <awen5@uci.edu>
2023-06-02 18:18:49 -0700

    recent versino

--- M2.py
+++ M2.py
@@ -94,8 +94,6 @@ if __name__ ==  "__main__":
     cos_sim_list = {}    
 
     for doc in top_urls:
-        print(count)
-        count += 1
         cos_sim = compute_cosine_similarities(query_score, all_doc_vector[doc])
         cos_sim_list[doc] = cos_sim
     end4 = time.time()
